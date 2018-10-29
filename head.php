83aa3ff5906f82cf98f1f8560ae0808a0a1bedc5
gledsondf <gledsondf@gmail.com>
2018-10-29 17:51:27 -0300

    boneco jaleco
    boneco jaleco

---
+++ head.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Referencial Nacional de Procedimentos Fisioterapêuticos - RNPF</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/rnpf.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+
+</head>
+<body>
+	<div id="geral" class="col-col row">
+		<div id="menu" class="col-col row">
+			<nav>
+				<ul class="lista-menu">
+					<li>nome 1</li>
+					<li>nome 2</li>
+				</ul>
+			</nav>
+		</div>
+		<section id="apresentacao" class="col-col row">
+			<article>
+				<div id="apresentacao-engloba" class="col-col row">
+					<div id="apresentacao-jaleco"></div>
+					<div id="apresentacao-conteudo"></div>
+				</div>
+			</article>
+		</section>
+		<section class="col-col row">
+			<article>
+				<header>
+					<h1>RNPF - Referencial Nacional de Procedimentos Fisioterapêuticos - RNPF 2018</h1>
+				</header>
+				<div class="aricle-conteudo" class="col-col row">
+					<h2>O que é O CHF?</h2>
+					<p>Os valores do referencial de remuneração dos procedimentos fisioterapêuticos estão expressos em reais, através da interpretação dos valores do Coeficiente de Honorários Fisioterapêuticos – CHF.</p>
+					<table>
+						<caption>CAPÍTULO I - CONSULTAS FISIOTERAPÊUTICAS</caption>
+						<thead>
+							<tr>
+								<th>Códigos RNPF</th>
+								<th>Descrição</th>
+								<th>Referencial CHF</th>
+								<th>Valor para 2018</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>12345678</td>
+								<td>asdf</td>
+								<td>asdf</td>
+								<td>asdf</td>
+							</tr>
+							<tr>
+								<td>12345678</td>
+								<td>asdf</td>
+								<td>asdf</td>
+								<td>asdf</td>
+							</tr>
+							<tr>
+								<td>12345678</td>
+								<td>asdf</td>
+								<td>asdf</td>
+								<td>asdf</td>
+							</tr>
+							<tr>
+								<td>12345678</td>
+								<td>asdf</td>
+								<td>asdf</td>
+								<td>asdf</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</article>
+		</section>
+		<section>
+			<div id="rodape" class="col-col row">
+				<div><a href="#geral" class="setaTopo" title="Para o topo">&circ;</a></div>
+				<a href="https://www.coffito.gov.br" title="Conselho Federal de Fisioterapia e Terapia Ocupacional" target="_blank"><img src="img/sistemacoffito.png"></a>
+			</div>
+		</section>
+	</div>
+</body>
+</html>
